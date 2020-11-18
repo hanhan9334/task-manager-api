22f833db2a91a372777b58c47d59461cf619ac8b
hanhan9334 <lhtony9334@126.com>
2020-11-18 01:15:29 -0500

    port changed to env.port

--- app.js
+++ app.js
@@ -358,6 +358,6 @@ const deleteTasksFromList = (_listId) => {
 
 }
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server running on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log("Server running");
 })
